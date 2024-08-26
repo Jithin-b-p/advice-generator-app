17fa0441f2403efee4796a070fce6422d3331c13
unknown <jithinbp007@gmail.com>
2024-08-26 23:59:28 +0530

    updating fetch call

--- src/components/AdvicePreview.tsx
+++ src/components/AdvicePreview.tsx
@@ -3,7 +3,20 @@ import { useEffect, useState } from "react";
 function AdvicePreview() {
   const [advice, setAdvice] = useState("");
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    async function apiCall() {
+      try {
+        const res = await fetch();
+
+        const { slip } = await res.json();
+        console.log(slip);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    apiCall();
+  }, []);
   return (
     <div className="bg-custNeutral-200 max-w-[25rem] text-center flex flex-col items-center p-5 rounded-xl">
       <span className="text-primary-neonGreen">Advice 102</span>
